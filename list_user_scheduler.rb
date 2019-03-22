aa3f20e0d100e59817ede24707f617fd1b2c686a
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-03-22 09:23:16 +0100

    update list user scheduler to list user"

--- list_user_scheduler.rb
+++ list_user_scheduler.rb
@@ -5,8 +5,13 @@ require 'rufus/scheduler'
 class ListUserScheduler < Sinatra::Base
   scheduler = Rufus::Scheduler.new
 
+  # scheduler.every '5s' do
+  #     puts "task is running"
+  # end
+
   scheduler.every '5s' do
-      puts "task is running"
+    slack_client = SlackClient.new
+    SlackUser.list_and_save_user(slack_client.default_client())
   end
 
 end
