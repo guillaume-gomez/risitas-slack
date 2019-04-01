0a17ecec863761ba14e9d228ac0dd8c2fced1fb6
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-04-01 08:01:12 +0200

    update where rake task is executed

--- list_user_scheduler.rb
+++ list_user_scheduler.rb
@@ -1,17 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'rufus/scheduler'
+require './models/slack_user'
 
 class ListUserScheduler < Sinatra::Base
   scheduler = Rufus::Scheduler.new
 
-  # scheduler.every '5s' do
-  #     puts "task is running"
-  # end
-
-  scheduler.every '5s' do
+  scheduler.every '1d' do
     slack_client = SlackClient.new
-    SlackUser.list_and_save_user(slack_client.default_client())
+    SlackUser.list_and_save_user(slack_client.client())
   end
 
 end
